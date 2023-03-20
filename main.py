0b2bed605256727e12e33e0028f3c8a0c3da5fb9
Dasun Thathsara <81586965+DasunThathsara@users.noreply.github.com>
2023-03-20 11:57:23 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,157 +1,186 @@
 # ----------------------------- RR -----------------------------
-def RR():
+def RR(data):
     global process_time
     value = 0
     count = 0
-    while sum(jobs) != 0:
-        for i in range(len(jobs)):
-            if jobs[i] == 0:
+    while sum(jobs[data]) != 0:
+        for i in range(len(jobs[data])):
+            if jobs[data][i] == 0:
                 continue
-
+            
             if count == time:
                 value = 1
                 break
 
-            jobs[i] -= 1
+            jobs[data][i] -= 1
             count += 1
             process_time += 1
-
-            if jobs[i] == 0:
-                turnaround_time[i] = process_time
+            
+            if jobs[data][i] == 0:
+                turnaround_time[data][i] = process_time
 
         if value:
             break
 
-    print("---------RR---------", " ".join([str(i) for i in jobs]))
+    print("---------RR---------", " ".join([str(i) for i in jobs[data]]))
+
 
 
 # -------------------------- SJF 01 --------------------------
-def SJF1():
+def SJF1(data):
     global process_time
     value = 0
     count = time
-    while sum(jobs) != 0:
+    while sum(jobs[data]) != 0:
         if count == 0:
             break
-
-        non_zero_list = [x for x in jobs if x != 0]
-        index = jobs.index(min(non_zero_list))
+        
+        non_zero_list = [x for x in jobs[data] if x != 0]
+        index = jobs[data].index(min(non_zero_list))
 
         if min(non_zero_list) >= count:
             process_time += count
-            jobs[index] -= count
+            jobs[data][index] -= count
             count = 0
 
-            if jobs[index] == 0:
-                turnaround_time[index] = process_time
-
+            if jobs[data][index] == 0:
+                turnaround_time[data][index] = process_time
+                
             break
 
-        process_time += jobs[index]
-        jobs[index] = 0
+
+        process_time += jobs[data][index]
+        jobs[data][index] = 0
         count -= min(non_zero_list)
+        
+        if jobs[data][index] == 0:
+            turnaround_time[data][index] = process_time
+            
+                    
 
-        if jobs[index] == 0:
-            turnaround_time[index] = process_time
+    print("--------SJF1--------", " ".join([str(i) for i in jobs[data]]))
 
-    print("--------SJF1--------", " ".join([str(i) for i in jobs]))
 
 
 # -------------------------- SJF 02 --------------------------
-def SJF2():
+def SJF2(data):
     global process_time
     value = 0
     count = time
-    while sum(jobs) != 0:
+    while sum(jobs[data]) != 0:
         if count == 0:
             break
-
-        non_zero_list = [x for x in jobs if x != 0]
-        index = jobs.index(min(non_zero_list))
+        
+        non_zero_list = [x for x in jobs[data] if x != 0]
+        index = jobs[data].index(min(non_zero_list))
 
         if min(non_zero_list) >= count:
             process_time += count
-            jobs[index] -= count
+            jobs[data][index] -= count
             count = 0
 
-            if jobs[index] == 0:
-                turnaround_time[index] = process_time
-
+            if jobs[data][index] == 0:
+                turnaround_time[data][index] = process_time
+                
             break
 
-        process_time += jobs[index]
-        jobs[index] = 0
+
+        process_time += jobs[data][index]
+        jobs[data][index] = 0
         count -= min(non_zero_list)
+        
+        if jobs[data][index] == 0:
+            turnaround_time[data][index] = process_time
+            
+                    
 
-        if jobs[index] == 0:
-            turnaround_time[index] = process_time
-
-    print("--------SJF2--------", " ".join([str(i) for i in jobs]))
+    print("--------SJF2--------", " ".join([str(i) for i in jobs[data]]))
+   
 
 
 # ---------------------------- FCFS ----------------------------
-def FCFS():
+def FCFS(data):
     global process_time
     value = 0
     count = time
-    for i in range(len(jobs)):
-        if count == 0 or sum(jobs) == 0:
+    for i in range(len(jobs[data])):        
+        if count == 0 or sum(jobs[data]) == 0:
             break
 
-        if jobs[i] == 0:
+        if jobs[data][i] == 0:
             continue
 
-        if jobs[i] >= count:
-            jobs[i] -= count
+        if jobs[data][i] >= count:
+            jobs[data][i] -= count
             process_time += count
             count = 0
-
-            if jobs[i] == 0:
-                turnaround_time[i] = process_time
-
+            
+            if jobs[data][i] == 0:
+                turnaround_time[data][i] = process_time
+                
             break
 
-        process_time += jobs[i]
-        count -= jobs[i]
-        jobs[i] = 0
+        process_time += jobs[data][i]
+        count -= jobs[data][i]
+        jobs[data][i] = 0
 
-        if jobs[i] == 0:
-            turnaround_time[i] = process_time
+        if jobs[data][i] == 0:
+            turnaround_time[data][i] = process_time
+            
 
-    print("--------FCFS--------", " ".join([str(i) for i in jobs]))
 
+    print("--------FCFS--------", " ".join([str(i) for i in jobs[data]]))
+
+
+def Scheduler():
+    for i in priority:
+        if sum(jobs[i]) != 0:
+            globals()[i](i)
+    
 
 if __name__ == "__main__":
-    jobs = [int(i) for i in input("Enter Burst Times: ").split()]
-    burst_time = jobs.copy()
     time = 20
-    turnaround_time = [0] * len(jobs)
-    waiting_time = [0] * len(jobs)
 
     process_time = 0
+    
 
-    priority = {'RR': -1, 'SJF1': -1, 'SJF2': -1, 'FCFS': -1}
+    priority = {'RR': 0, 'SJF1': 1, 'SJF2': 2, 'FCFS': 3}
+    jobs = {'RR': [], 'SJF1': [], 'SJF2': [], 'FCFS': []}
+    
     for i in priority:
-        print("Enter Priority for", i, end=" : ")
-        priority[i] = int(input())
+        jobs[i] = [int(i) for i in input("Enter values for " + i + "(priority level " + str(priority[i]) + "): ").split()]
 
-    priority = dict(sorted(priority.items(), key=lambda item: item[1]))
+    turnaround_time = {'RR': [0] * len(jobs['RR']), 'SJF1': [0] * len(jobs['SJF1']), 'SJF2': [0] * len(jobs['SJF2']), 'FCFS': [0] * len(jobs['FCFS'])}
+    waiting_time = {'RR': [0] * len(jobs['RR']), 'SJF1': [0] * len(jobs['SJF1']), 'SJF2': [0] * len(jobs['SJF2']), 'FCFS': [0] * len(jobs['FCFS'])}
+    burst_time = {'RR': jobs['RR'].copy(), 'SJF1': jobs['SJF1'].copy(), 'SJF2': jobs['SJF2'].copy(), 'FCFS': jobs['FCFS'].copy()}
+
+
     print()
-
-    print("-----Burst Time-----", " ".join([str(i) for i in burst_time]))
-
+    
     while True:
-        if sum(jobs) == 0:
+        if sum(jobs["RR"]) == 0 and sum(jobs["SJF1"]) == 0 and sum(jobs["SJF2"]) == 0 and sum(jobs["FCFS"]) == 0:
             break
 
-        for i in priority:
-            if sum(jobs) != 0:
-                globals()[i]()
+        Scheduler()
+        
 
-    for i in range(len(turnaround_time)):
-        waiting_time[i] = turnaround_time[i] - burst_time[i]
+    for i in jobs:
+        for j in range(len(turnaround_time[i])):
+            waiting_time[i][j] = turnaround_time[i][j] - burst_time[i][j]
 
     print("\n")
-    print("Burst Time   :", " ".join([str(i) for i in burst_time]))
-    print("Consume Time :", " ".join([str(i) for i in turnaround_time]))
-    print("Waiting Time :", " ".join([str(i) for i in waiting_time]))
+    print("Burst Times:")
+    for i in priority:
+        print("Priority level", priority[i], ":", ", ".join(str(i) for i in burst_time[i]))
+
+    print()
+
+    print("Consume Times:")
+    for i in priority:
+        print("Priority level", priority[i], ":", ", ".join(str(i) for i in turnaround_time[i]))
+
+    print()
+
+    print("Waiting Times:")
+    for i in priority:
+        print("Priority level", priority[i], ":", ", ".join(str(i) for i in waiting_time[i]))
